e69dfe04ab8e05a839645fc9fc95b1d8c93395eb
vivek7kushwah <vivekkushwah1412@gmail.com>
2026-03-11 09:49:19 +0530

    some changes

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -48,7 +48,7 @@ export const register = async (req: Request, res: Response) => {
 export const login = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
-
+    console.log("Login request:", { email, password });
     const user = await prisma.user.findUnique({ where: { email } }); // see if user exist
     if (!user) {
       return res.status(400).json({ message: "Invalid email or password" });
